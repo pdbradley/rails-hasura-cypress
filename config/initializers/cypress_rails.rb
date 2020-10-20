fa8a661756f68ee4fbf3ca545b89b1afba18dcb0
Philip Bradley <pdbradley@gmail.com>
2020-10-20 10:25:26 -0400

    add cypress test to test dogs page

---
+++ config/initializers/cypress_rails.rb
@@ -0,0 +1,20 @@
+return unless Rails.env.test?
+
+Rails.application.load_tasks unless defined?(Rake::Task)
+
+CypressRails.hooks.before_server_start do
+  # Called once, before either the transaction or the server is started
+end
+
+CypressRails.hooks.after_transaction_start do
+  # Called after the transaction is started (at launch and after each reset)
+  spot = Dog.create(name: 'Spot', breed: 'Pug')
+end
+
+CypressRails.hooks.after_state_reset do
+  # Triggered after `/cypress_rails_reset_state` is called
+end
+
+CypressRails.hooks.before_server_stop do
+  # Called once, at_exit
+end
---
+++ cypress/integration/dogs.spec.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress" />
+
+context('Dogs', () => {
+  beforeEach(() => {
+    cy.visit('/dogs');
+  });
+
+  it('Creates a Dog', () => {
+    cy.contains('New Dog').click();
+    cy.get('#dog_name').type('Beans');
+    cy.get('#dog_breed').type('Mutt');
+    cy.contains('Create Dog').click();
+    cy.contains('Back').click();
+
+    cy.location('pathname').should('eq', '/dogs');
+    cy.contains('Beans').should('be.visible');
+    cy.contains('Mutt').should('be.visible');
+  });
+});
