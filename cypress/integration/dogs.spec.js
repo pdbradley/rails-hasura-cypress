5caf68f64eda689b4fa7f06a8c667fe5e7e693a7
Philip Bradley <pdbradley@gmail.com>
2020-10-27 10:36:41 -0400

    cypress aggrid selectors for the win

--- cypress/integration/dogs.spec.js
+++ cypress/integration/dogs.spec.js
@@ -5,14 +5,13 @@ context('Dogs', () => {
     cy.visit('/dogs');
   });
 
-  it('Creates a new Dog', () => {
-    // verify Ag Grid boilerplate
-    cy.contains('Toyota').should('be.visible');
-
+  it('Verifies Fixture Data', () => {
     // verify fixture is there already
     cy.contains('Spot').should('be.visible');
     cy.contains('Pug').should('be.visible');
+  });
 
+  it('Creates a new Dog', () => {
     cy.contains('New Dog').click();
     cy.get('#dog_name').type('Beans');
     cy.get('#dog_breed').type('Mutt');
@@ -22,6 +21,33 @@ context('Dogs', () => {
     cy.location('pathname').should('eq', '/dogs');
     cy.contains('Beans').should('be.visible');
     cy.contains('Mutt').should('be.visible');
-    cy.contains('Toyota').should('be.visible');
   });
+
+  it('Verifies Data within Ag Grid', () => {
+    assertCellValueInRowColumnId(0, 'make', 'Toyota');
+    assertCellValueInRowColumnId(0, 'model', 'Celica');
+    assertCellValueInRowColumnId(0, 'price', '35000');
+
+    assertCellValueInRowColumnId(1, 'make', 'Ford');
+    assertCellValueInRowColumnId(1, 'model', 'Mondeo');
+    assertCellValueInRowColumnId(1, 'price', '32000');
+  });
+
+  function assertCellValueInFirstRow(colId, value) {
+    cy.get('.ag-center-cols-container .ag-row')
+      .first()
+      .find(`[col-id="${colId}"]`)
+      .then(cell => {
+        expect(cell).to.have.text(value);
+      });
+  }
+
+  function assertCellValueInRowColumnId(row, colId, value) {
+    cy.get('.ag-center-cols-container .ag-row')
+      .eq(row)
+      .find(`[col-id="${colId}"]`)
+      .then(cell => {
+        expect(cell).to.have.text(value);
+      });
+  }
 });
